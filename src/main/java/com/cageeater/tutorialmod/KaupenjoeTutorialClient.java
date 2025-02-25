03b737785cb081ed93c7a752d461fe8fe592d3b2
Carloh2s3 <carwesh2s3@gmail.com>
2025-02-25 17:52:43 +0100

    Initial commit

---
+++ src/main/java/com/cageeater/tutorialmod/KaupenjoeTutorialClient.java
@@ -0,0 +1,10 @@
+package com.cageeater.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KaupenjoeTutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
